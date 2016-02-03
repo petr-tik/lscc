f99d005069abd77fab4912609ed4db4d52970152
petr-tik <tikilyaynen@gmail.com>
2016-02-03 22:03:25 +0000

    added the rotate method. using modular arithmetic, implemented a rotate method, that takes an argument way and changes the direction attribute

--- mars_kata.py
+++ mars_kata.py
@@ -1,4 +1,8 @@
 #! usr/bin/env python
+"""
+from london software craftsmanship group meetup on python katas
+"""
+
 import unittest
 
 class Rover(object):
@@ -7,7 +11,17 @@ class Rover(object):
         self.y = 0
         self.direction = "N"
 
-    def move(self, direction):
+    def rotate(self, way):
+        options = ["N", "W", "S", "E"]
+        if way == "L":
+            idx_l = (options.index(self.direction) + 1) % 4        
+            self.direction = options[idx_l]            
+
+        elif way == "R":
+            idx_r = ((options.index(self.direction) - 1) % 4)
+            self.direction = options[idx_r]
+
+    def move(self):
         if self.direction == "N":
             self.y += 1
             return self.y
@@ -20,6 +34,19 @@ class Rover(object):
         elif self.direction == "E":
             self.x += 1
             return self.x
+
+
+
+
+r = Rover(0,0,"N")
+for x in xrange(10):
+
+    print "and now I am facing {}".format(r.direction)
+    print "rotating right"
+    r.rotate("R")
+    print "and now I am facing {}".format(r.direction)
+    
+
 
 
 class testRover(unittest.TestCase):
@@ -35,16 +62,22 @@ class testRover(unittest.TestCase):
         self.assertEqual(self.x, 0)
 
     def test_move_N(self):
-        self.move_N = self.rover.move("N")
-        self.assertEqual(self.move_N, self.rover.y)    
+        move_N = self.rover.move()
+        self.assertEqual(move_N, self.rover.y)    
         
     def test_move_W(self):
- #       self.move_W = self.rover.move("W")
- #       self.assertEqual(self.move_W, self.rover.x)    
+#        move_W = self.rover.move()
+ #       self.assertEqual(move_W, self.rover.x)    
         pass
+
+
+
     def test_move_E(self):
-        #self.move_E = self.rover.move("E")
-        #self.assertEqual(self.move_E, self.rover.x)    
+        #move_E = self.rover.move()
+        #self.assertEqual(move_E, self.rover.x)    
+        pass
+
+    def test_rotate(self):
         pass
 
 
