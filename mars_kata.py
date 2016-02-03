786b747e2fd5dec1fae7cb71714290ab18576316
petr-tik <tikilyaynen@gmail.com>
2016-02-03 19:41:32 +0000

    started move

--- mars_kata.py
+++ mars_kata.py
@@ -1,26 +1,31 @@
 #! usr/bin/env python
 import unittest
 
-class easy(unittest.TestCase):
-    pass
-
 class Rover(object):
-    def __init__(self, position, direction):
-        self.position = (0,0)
+    def __init__(self, x, y, direction):
+        self.x = 0
+        self.y = 0
         self.direction = "N"
+
+    def move(self):
+        pass
+
 
 
 class testRover(unittest.TestCase):
     def setUp(self):
-        self.rover = Rover((0,0), "N")
+        self.rover = Rover(0, 0, "N")
 
     def test_dir(self):        
         self.direction = self.rover.direction
         self.assertEqual(self.direction, "N")
 
     def test_pos(self):
-        self.position = self.rover.position
-        self.assertEqual(self.position, (0,0))
+        self.x = self.rover.x
+        self.assertEqual(self.x, 0)
+
+    def test_move(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
