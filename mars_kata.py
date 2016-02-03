8c1df2d9f8ed8c3e41f48aabfc876c576690d175
petr-tik <tikilyaynen@gmail.com>
2016-02-03 19:03:48 +0000

    rover

--- mars_kata.py
+++ mars_kata.py
@@ -4,6 +4,17 @@ import unittest
 class easy(unittest.TestCase):
     pass
 
+class Rover(object):
+    def __init__(self, position, direction):
+        self.position = (0,0)
+        self.direction = N
+
+
+
+class testRover(unittest.TestCase):
+    pass    
+
+
 if __name__ == '__main__':
     unittest.main()
 
